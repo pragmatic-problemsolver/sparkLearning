11a40db32015aeced9ab03e059c65ff65343383a
vaibhavgs <gs.vaibhav@gmail.com>
2024-04-14 19:33:17 +0530

    map operation using RDD

--- Project/src/main/java/com/virtualpairprogrammers/MonolithClass.java
+++ Project/src/main/java/com/virtualpairprogrammers/MonolithClass.java
@@ -5,18 +5,18 @@ import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.sources.In;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MonolithClass {
     public static void main(String[] args) {
-        List<Double> inputData = new ArrayList<>();
-        inputData.add(1.0);
-        inputData.add(2.0);
-        inputData.add(3.0);
-        inputData.add(4.0);
-        inputData.add(5.0);
+        List<Integer> inputData = new ArrayList<>();
+        inputData.add(35);
+        inputData.add(12);
+        inputData.add(90);
+        inputData.add(20);
 
         Logger.getLogger("org.apache").setLevel(Level.WARN);
         SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");//use the local configuration and * indicates to use all the cores available on the local machine
@@ -24,10 +24,13 @@ public class MonolithClass {
         //SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local");//use the local configuration and * indicates to use all the cores available on the local machine
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<Double> myRdd = sc.parallelize(inputData);
+        JavaRDD<Integer> myRdd = sc.parallelize(inputData);
 
-        Double result = myRdd.reduce((value1, value2) -> value1+value2);
-        System.out.println(result);
+        Integer result = myRdd.reduce((value1, value2) -> value1+value2);
+        JavaRDD<Double> sqrtRdd = myRdd.map(value->Math.sqrt(value));
+
+//        System.out.println(sqrtRdd); //this actually is a map and not the value by itself.
+
         sc.close();
     }
 }
